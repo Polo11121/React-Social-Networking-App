1e29a21d86930f37e60d964ef1a84e9e5fb6c035
Michał Jasiński <michal.jasinski11121.com>
2022-11-26 14:18:52 +0100

    Fix change email on prod

--- src/pages/Profile/ProfileDetails/ProfileDetailsChangePassword/ChangePasswordSchema.ts
+++ src/pages/Profile/ProfileDetails/ProfileDetailsChangePassword/ChangePasswordSchema.ts
@@ -3,11 +3,19 @@ import * as Yup from 'yup';
 export const ChangePasswordSchema = Yup.object().shape({
   passwordCurrent: Yup.string().required('Wpisz stare hasło'),
   password: Yup.string()
+    .test(
+      'same passwords',
+      'Nowe hasło powinno być inne niż obecnie ustawione',
+      (value, ctx) =>
+        Boolean(
+          value && ctx.parent.passwordCurrent !== value && value.length > 7
+        )
+    )
     .min(8, 'Hasło musi mieć conajmniej 8 znaków')
     .required('Hasło musi mieć conajmniej 8 znaków'),
   passwordConfirm: Yup.string().test(
     'equals passwords',
-    'Nieprawidłowe hasło',
+    'Nieprawidłowe powtórzenie hasło',
     (value, ctx) =>
       Boolean(value && ctx.parent.password === value && value.length > 7)
   ),
